e6b01489b01b0000d9891c273c90a9f22bd3966b
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2024-07-23 20:41:54 +0530

    local vaiable

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$var1 = "hello";
+$var2 = "world";
+echo $var1."</br>";
+echo $var2."</br>";
+
+//Loacl Variable
+
+function marks () {
+    $marks = 90;
+    echo "local variable marks is : " .$marks;
+}
+
+marks();
+
+?>

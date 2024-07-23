94b367b4ab0a738fb8ac24076dfa41ba9e649c18
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2024-07-23 21:21:37 +0530

    global variable

--- index.php
+++ index.php
@@ -2,16 +2,26 @@
 
 $var1 = "hello";
 $var2 = "world";
-echo $var1."</br>";
-echo $var2."</br>";
+echo $var1 . "</br>";
+echo $var2 . "</br>";
 
-//Loacl Variable
+//Local Variable
 
-function marks () {
+function marks()
+{
     $marks = 90;
-    echo "local variable marks is : " .$marks;
+    echo "local variable marks is : " . $marks;
+    echo "</br>";
 }
-
 marks();
 
-?>
+//Global variable
+$score = 50;
+
+function scorecard()
+{
+    global $score;
+    echo "global variable score is : " . $score;
+}
+
+scorecard();

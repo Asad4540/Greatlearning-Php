dc16589c0553fbfe475eac3dda8d6e362c9a2bb2
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2024-07-24 02:56:52 +0530

    loops added

---
+++ loops.php
@@ -0,0 +1,22 @@
+<?php
+
+//for loop
+echo "for loop</br>";
+for ($a = 0; $a < 10; $a++) {
+    echo $a;
+    echo "</br>";
+}
+
+//while loop
+echo"</br>";
+echo"</br>";
+echo"while loop</br>";
+
+
+
+$a=0;
+while($a<7){
+    echo $a;
+    echo"</br>";
+    $a++;
+}

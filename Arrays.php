7c184dbd11108fe78f900ba28e7c8663f393e3fc
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2024-08-07 02:29:33 +0530

    array added

---
+++ Arrays.php
@@ -0,0 +1,14 @@
+<?php
+
+//Indexed Array
+
+$players = array("Dhoni", "Kohli", "Rohit");
+echo "Player 1 is $players[0] </br>";
+echo "Player 2 is $players[1] </br>";
+echo "Player 3 is $players[2] </br></br>";
+
+//Multi-Dimensioanl Array
+
+$students = array(array("Ram", "Shyam", "Ghanshyam"), array("93", "95", "82"), array("1", "2", "3"));
+print_r($students);
+

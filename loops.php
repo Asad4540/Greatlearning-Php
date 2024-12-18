fc1fdd669aa8d0a1a0baab2961e09568d0e9e3f0
Asad Chaudhary <100398153+Asad4540@users.noreply.github.com>
2024-12-18 16:58:28 +0530

    Update loops.php

--- loops.php
+++ loops.php
@@ -19,3 +19,5 @@ while($a<7){
     echo"</br>";
     $a++;
 }
+
+//https://vereigen-my.sharepoint.com/:f:/g/personal/vidyadhar_sutar_vereigenmedia_com/EvH0eEFKT8VGnNCWnZGNS80Bol55aWi9oD0r97j3qcdtAA?e=3wkR5D
